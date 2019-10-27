66e7ee6743172472bf7afe4597a3c7d49bab2b16
auditt98 <vietanh8i1998@gmail.com>
2019-10-27 18:30:12 +0700

    ?

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const path = require('path')
 const port = process.env.port
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + "/simpleGrid.html")))
-app.listen(port, () => console.log(`Example app listening on port port!`))
+app.listen(process.env.PORT, () => console.log(`Example app listening on port port!`))
