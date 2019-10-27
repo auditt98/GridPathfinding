ace471552f93d45c93cd5c70a5c405d9564a4803
auditt98 <vietanh8i1998@gmail.com>
2019-10-27 18:27:24 +0700

    fix port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const port = 3000
+const port = process.env.port
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + "/simpleGrid.html")))
 app.listen(port, () => console.log(`Example app listening on port port!`))
